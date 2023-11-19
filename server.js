01ecd18d7d64641aeeaa090fb1291a4006a3c553
Fabian Stiven Valencia Cordoba <fabian.cordoba@correounivalle.edu.co>
2023-11-19 13:58:46 -0500

    Version 0.1.0 Web RTC of the EISC Metaverse

---
+++ server.js
@@ -0,0 +1,54 @@
+import { Server } from "socket.io";
+
+const io = new Server({
+  cors: {
+    origin: ["http://localhost:3000", "https://eisc-metaverse.vercel.app"]
+  },
+});
+
+io.listen(5000);
+
+let peers = {};
+
+io.on("connection", (socket) => {
+  console.log(
+    "Peer joined with ID",
+    socket.id,
+    ". There are " +
+    io.engine.clientsCount +
+    " peer(s) connected."
+  );
+
+  if (!peers[socket.id]) {
+    peers[socket.id] = {};
+  }
+
+  socket.emit( "introduction", Object.keys(peers));
+
+  io.emit("newUserConnected", socket.id);
+
+  socket.on("signal", (to, from, data) => {
+    if (to in peers) {
+      io.to(to).emit("signal", to, from, data);
+    } else {
+      console.log("Peer not found!");
+    }
+  });
+
+  socket.on("disconnect", () => {
+    delete peers[socket.id];
+    io.sockets.emit(
+      "userDisconnected",
+      io.engine.clientsCount,
+      socket.id,
+      Object.keys(peers)
+    );
+    console.log(
+      "User " +
+      socket.id +
+      " diconnected, there are " +
+      io.engine.clientsCount +
+      " clients connected"
+    );
+  });
+});
